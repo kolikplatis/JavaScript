bad70daf7e45ce623a82bf392ddcf9d3aa2c90bd
unknown <kolikplatis@gmail.com>
2024-11-08 15:36:20 +0300

    rename task1.js

---
+++ JS_1.js
@@ -0,0 +1,133 @@
+//ШАГИ 1-25:
+// let item_1;
+// item_1 = 5;
+// console.log(item_1);
+
+// let item_2;
+// item_2 = 3;
+// console.log(item_2);
+
+// let item_3;
+// item_3 = item_1 + item_2;
+// console.log(item_3);
+
+// let item_4;
+// item_4 = "Yolochka";
+// console.log(item_4);
+
+// console.log(item_3+item_4);
+
+// console.log(item_3*item_4);
+
+// let item_5;
+// item_5=item_3;
+
+// let item_6;
+// item_6=15;
+
+// let item_6_type;
+
+// item_6_type = typeof item_6;
+
+// console.log("——","item_6 ==",item_6+",","item_6_type ==",item_6_type,"——");
+
+// let item_7 = String(item_6);
+
+// let item_7_type;
+// item_7_type = typeof item_7;
+
+// console.log("——","item_7 ==",item_7+",","item_7_type ==",item_7_type,"——")
+
+
+// ШАГИ 26-33:
+// let age_1 = 10;
+// let age_2 = 18;
+// let age_3 = 60;
+
+// if (age_1 < age_2){
+//     console.log("You don’t have access cause your age is " + age_1 + " It’s less then");
+// } else if (age_1 >= age_2 && age_1 < age_3) {
+//     console.log("Welcome !");
+// } else if (age_1 > age_3) {
+//     console.log("Keep calm and look Culture channel");
+// } else {
+//     console.log("Technical work");
+//}
+
+
+
+// Задание 1*
+// const checkAge = function age(age_1, age_2, age_3) {
+//     if (age_1 < age_2) {
+//         console.log("You don’t have access cause your age is " + age_1 + " It’s less than " + age_2);
+//     } else if (age_1 >= age_2 && age_1 < age_3) {
+//         console.log("Welcome !");
+//     } else if (age_1 >= age_3) {
+//         console.log("Keep calm and look Culture channel");
+//     } else {
+//         console.log("Technical work");
+//         }
+// };
+
+// checkAge(17, 18, 61);
+
+
+
+// Задание 2*
+// const checkAge = function age(age_1, age_2, age_3) {
+//     if (typeof age_1 != "number" || typeof age_2 != "number" || typeof age_3 != "number") {
+//         console.log("Error. Non valid type!");
+//         return;
+//     }
+//     if (age_1 < age_2) {
+//         console.log("You don’t have access cause your age is " + age_1 + " It’s less than " + age_2);
+//     } else if (age_1 >= age_2 && age_1 < age_3) {
+//         console.log("Welcome !");
+//     } else if (age_1 >= age_3) {
+//         console.log("Keep calm and look Culture channel");
+//     } else {
+//         console.log("Technical work");
+//         }
+// };
+
+// checkAge(17, 18, 61);
+
+
+//Задание 3**
+const checkAge = function age(age_1, age_2, age_3) {
+age_1 = +age_1;
+age_2 = +age_2;
+age_3 = +age_3;
+    if (age_1 < age_2) {
+        console.log("You don’t have access cause your age is " + age_1 + " It’s less than " + age_2);
+    } else if (age_1 >= age_2 && age_1 < age_3) {
+        console.log("Welcome !");
+    } else if (age_1 >= age_3) {
+        console.log("Keep calm and look Culture channel");
+    } else {
+        console.log("Technical work");
+        }
+};
+
+checkAge("17", "18", "61");
+
+//Задание 4**
+const checkAge = function age(age_1) {
+const age_2 = 18;
+const age_3 = 61;
+age_1 = prompt();
+age_1 = +age_1;
+    if (age_1 < age_2) {
+         console.log("You don’t have access cause your age is " + age_1 + " It’s less than " + age_2);
+    } else if (age_1 >= age_2 && age_1 < age_3) {
+        console.log("Welcome !");
+    } else if (age_1 >= age_3) {
+         console.log("Keep calm and look Culture channel");
+    } else {
+        console.log("Technical work");
+        }
+    };
+
+checkAge();
+
+
